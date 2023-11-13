d7717027644b04621b12395933327fb91684e35e
RhitikRaj <rhitikraj0035.rr@gmail.com>
2023-11-13 21:00:18 +0100

    Final updation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',  # Name of your package
+    version='1.0',  # Version number
+    packages=find_packages(),  # Automatically discover and include all packages in the package directory
+    install_requires=[],  # List any external dependencies here
+    author='Prateek',  # Your name
+    author_email='2404.prateek@fau.de',  # Your email address
+    description='A simple math quiz package for DSSS assignment',  # Short description of your package
+    url='https://github.com/yourusername/math_quiz',  # URL to the package's source code repository
+    classifiers=[]
+)
